fb65c4ee31fdba3a52f7e24a2e597e9c1803c546
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-11-24 23:27:51 +0700

    Update modulated_deform_conv_func.py

--- functions/modulated_deform_conv_func.py
+++ functions/modulated_deform_conv_func.py
@@ -2,9 +2,9 @@ import torch
 from torch import nn
 from .dcn import DeformableConv2d
 
-class ModulatedDeformConv(nn.Module):
+class ModulatedDeformConvFunction(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, deformable_groups=1, im2col_step=64, bias=True):
-        super(ModulatedDeformConv, self).__init__()
+        super(ModulatedDeformConvFunction, self).__init__()
         
         self.dcn = DeformableConv2d(
             in_channels=in_channels,
